674736f029bb31b53210f25ceb156f04347c578b
Harsh Dhama <dhamaharsh9@gmail.com>
2023-10-01 12:14:56 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -4,41 +4,21 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 
-import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Cart from './features/cart/Cart';
 import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
 import ProductDetailPage from './pages/ProductDetailPage';
 import Protected from './features/auth/components/Protected';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  checkAuthAsync,
-  selectLoggedInUser,
-  selectUserChecked,
-} from './features/auth/authSlice';
+import { selectLoggedInUser } from './features/auth/authSlice';
 import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
-import PageNotFound from './pages/404';
-import OrderSuccessPage from './pages/OrderSuccessPage';
-import UserOrdersPage from './pages/UserOrdersPage';
-import UserProfilePage from './pages/UserProfilePage';
-import { fetchLoggedInUserAsync } from './features/user/userSlice';
-import Logout from './features/auth/components/Logout';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ProtectedAdmin from './features/auth/components/ProtectedAdmin';
-import AdminHome from './pages/AdminHome';
-import AdminProductDetailPage from './pages/AdminProductDetailPage';
-import AdminProductFormPage from './pages/AdminProductFormPage';
-import AdminOrdersPage from './pages/AdminOrdersPage';
-import { positions, Provider } from 'react-alert';
-import AlertTemplate from 'react-alert-template-basic';
-import StripeCheckout from './pages/StripeCheckout';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-
-const options = {
-  timeout: 5000,
-  position: positions.BOTTOM_LEFT,
-};
-
 const router = createBrowserRouter([
   {
     path: '/',
@@ -46,14 +26,6 @@ const router = createBrowserRouter([
       <Protected>
         <Home></Home>
       </Protected>
-    ),
-  },
-  {
-    path: '/admin',
-    element: (
-      <ProtectedAdmin>
-        <AdminHome></AdminHome>
-      </ProtectedAdmin>
     ),
   },
   {
@@ -88,116 +60,23 @@ const router = createBrowserRouter([
       </Protected>
     ),
   },
-  {
-    path: '/admin/product-detail/:id',
-    element: (
-      <ProtectedAdmin>
-        <AdminProductDetailPage></AdminProductDetailPage>
-      </ProtectedAdmin>
-    ),
-  },
-  {
-    path: '/admin/product-form',
-    element: (
-      <ProtectedAdmin>
-        <AdminProductFormPage></AdminProductFormPage>
-      </ProtectedAdmin>
-    ),
-  },
-  {
-    path: '/admin/orders',
-    element: (
-      <ProtectedAdmin>
-        <AdminOrdersPage></AdminOrdersPage>
-      </ProtectedAdmin>
-    ),
-  },
-  {
-    path: '/admin/product-form/edit/:id',
-    element: (
-      <ProtectedAdmin>
-        <AdminProductFormPage></AdminProductFormPage>
-      </ProtectedAdmin>
-    ),
-  },
-  {
-    path: '/order-success/:id',
-    element: (
-      <Protected>
-        <OrderSuccessPage></OrderSuccessPage>{' '}
-      </Protected>
-    ),
-  },
-  {
-    path: '/my-orders',
-    element: (
-      <Protected>
-        <UserOrdersPage></UserOrdersPage>{' '}
-      </Protected>
-    ),
-  },
-  {
-    path: '/profile',
-    element: (
-      <Protected>
-        <UserProfilePage></UserProfilePage>{' '}
-      </Protected>
-    ),
-  },
-  {
-    path: '/stripe-checkout/',
-    element: (
-      <Protected>
-        <StripeCheckout></StripeCheckout>
-      </Protected>
-    ),
-  },
-  {
-    path: '/logout',
-    element: <Logout></Logout>,
-  },
-  {
-    path: '/forgot-password',
-    element: <ForgotPasswordPage></ForgotPasswordPage>,
-  },
-  {
-    path: '/reset-password',
-    element: <ResetPasswordPage></ResetPasswordPage>,
-  },
-  {
-    path: '*',
-    element: <PageNotFound></PageNotFound>,
-  },
 ]);
 
 function App() {
+
   const dispatch = useDispatch();
   const user = useSelector(selectLoggedInUser);
-  const userChecked = useSelector(selectUserChecked);
 
-  useEffect(() => {
-    dispatch(checkAuthAsync());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (user) {
-      dispatch(fetchItemsByUserIdAsync());
-      // we can get req.user by token on backend so no need to give in front-end
-      dispatch(fetchLoggedInUserAsync());
+  useEffect(()=>{
+    if(user){
+      dispatch(fetchItemsByUserIdAsync(user.id))
     }
-  }, [dispatch, user]);
+  },[dispatch, user])
 
   return (
-    <>
-      <div className="App">
-        {userChecked && (
-          <Provider template={AlertTemplate} {...options}>
-            <RouterProvider router={router} />
-          </Provider>
-        )}
-        {/* Link must be inside the Provider */}
-      </div>
-    </>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
